7bf62b158f3020322379fb465494539f5a1dbac9
Mimbao <chowjenghan@gmail.com>
2026-08-27 15:50:51 +0800

    Add date comprehension

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,24 @@
-public class Deadline extends Task {
-    private final String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    private final LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
-    public String getBy() {
+    public LocalDateTime getBy() {
         return by;
     }
 
     @Override
     public String toString() {
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm a");
+
         return "[D][" + getStatusIcon() + "] " + getDescription()
-                + " (by: " + by + ")";
+                + " (by: " + by.format(formatter) + ")";
     }
 }
